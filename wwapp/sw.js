2cd8a40cfc8160730bdf1c7b5d1684e1851192c7
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-07-11 11:48:12 +0530

    Create sw.js

---
+++ wwapp/sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "H book",
+  "short_name": "H book",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#0f172a",
+  "icons": [
+    {
+      "src": "https://i.ibb.co/qYXDbTTT/H-book-logo-11zon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://i.ibb.co/qYXDbTTT/H-book-logo-11zon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
